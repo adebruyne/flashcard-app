416cf4ac33ea76938c028070518e701be0f202be
Aylin DeBruyne <adebruyne88@gmail.com>
2018-06-28 09:33:41 -0400

    added some example-queries.sql

---
+++ example-queries.sql
@@ -0,0 +1,17 @@
+--We have a new user who wants to sign up 
+--We have a user who wants to create a new deck 
+--We have a user who wants to create a new card in a deck (*)
+
+--user selects a deck
+--select a card from deckid
+
+--update a deck
+--update a card from deck
+
+--delete a card from a deck
+--delete a deck from a user
+
+
+
+
+
